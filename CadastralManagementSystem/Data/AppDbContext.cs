fc853b9d18137ecd775d9d5263cf76588ec69ca9
AlexGalkin <galalexzv@gmail.com>
2024-11-17 16:25:28 +0300

    Initial commit: added project files

---
+++ CadastralManagementSystem/Data/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class Property
+{
+    public int Id { get; set; }
+    public string? Address { get; set; }
+    public string? CadastralNumber { get; set; }
+    public string? Description { get; set; }
+    public double Area { get; set; }
+    public string? Owner { get; set; }
+}
+
+public class Proposal
+{
+    public int Id { get; set; }
+    public string? Address { get; set; }
+    public string? CadastralNumber { get; set; }
+    public string? Description { get; set; }
+    public double Area { get; set; }
+    public string? Owner { get; set; }
+}
+
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Property>? Properties { get; set; }
+    public DbSet<Proposal>? Proposals { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+}
---
+++ CadastralManagementSystem/Migrations/20241117131150_InitialCreate.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CadastralManagementSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Properties",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CadastralNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Area = table.Column<double>(type: "float", nullable: false),
+                    Owner = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Properties", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Proposals",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CadastralNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Area = table.Column<double>(type: "float", nullable: false),
+                    Owner = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Proposals", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Properties");
+
+            migrationBuilder.DropTable(
+                name: "Proposals");
+        }
+    }
+}
